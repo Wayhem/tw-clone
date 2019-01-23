65fdf41715785ca8e2a63b5157dcf88b10886307
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-22 23:14:43 -0400

    react router set with components for the streams CRUD

---
+++ client/src/components/streams/StreamDelete.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StreamDelete = () => {
+    return <div>strimdelete</div>
+}
+
+export default StreamDelete;
---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StreamEdit = () => {
+    return <div>strimedit</div>
+}
+
+export default StreamEdit;
