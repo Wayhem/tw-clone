98f1acc33b7c7164fc2434f0d4ccc67bd6adbe18
Jose Villarroel <oscarclarke9900@gmail.com>
2019-02-06 23:12:01 +0100

    create stream now redirects home after action, made custom history object which now allows to push routes from action creators.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
     EDIT_STREAM
 } from './types';
 import streams from '../apis/streams';
+import history from '../history';
 
 export const signIn = id => {
     return {
@@ -39,6 +40,7 @@ export const createStream = formValues => async (dispatch, getState) => {
     const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({ type: CREATE_STREAM, payload: response.data })
+    history.push('/');
 }
 
 export const editStream = (id, data) => async dispatch => {
