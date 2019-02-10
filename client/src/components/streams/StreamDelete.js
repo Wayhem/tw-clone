c5074ef7545491b2f67fce86900e91ee72cae77e
Jose Villarroel <oscarclarke9900@gmail.com>
2019-02-10 13:55:01 +0100

    used portals to show modal and hide modal.

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Modal from '../Modal';
+
 const StreamDelete = () => {
-    return <div>strimdelete</div>
+    return (
+        <div>
+            StreamDelete
+            <Modal />
+        </div>
+    );
 }
 
 export default StreamDelete;
