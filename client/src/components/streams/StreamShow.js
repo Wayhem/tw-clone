8dce4e77f9ee81c1e04e42b84fb26b2c9cd8c16e
Jose Villarroel <oscarclarke9900@gmail.com>
2019-02-13 20:11:47 +0100

    unmount now removes player config

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,24 +1,67 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
+import flv from 'flv.js';
 
 import {fetchStream} from '../../actions';
 
 class StreamShow extends Component {
-    componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef();
     }
 
-    render() {
+    componentDidMount() {
+        const {id} = this.props.match.params;
+        this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();  
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const {id} = this.props.match.params;
+
+        this.player = flv.createPlayer({
+            type:'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+    }
+
+    renderText(){
         if(!this.props.stream) {
             return <div>Loading...</div>
         }
+
         const { title, description } = this.props.stream
+
         return (
-            <div>
+            <Fragment>
                 <h1>{title}</h1>
                 <h5>{description}</h5>
+            </Fragment>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <video ref={this.videoRef} style={{width: '100%'}} controls />
+                {this.renderText()}
             </div>
         );
+    }
+
+    componentWillMount() {
+        this.player.destroy();
     }
 } 
 
